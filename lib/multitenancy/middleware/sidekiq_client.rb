68ffa5859c1679475783daaa18223f862756c07c
Kent Mewhort <kent@openissues.ca>
2015-08-10 10:47:33 -0700

    Only run sidekiq jobs with a tenant if one is specified.

--- lib/multitenancy/middleware/sidekiq_client.rb
+++ lib/multitenancy/middleware/sidekiq_client.rb
@@ -1,8 +1,11 @@
 module Multitenancy::Middleware
   class SidekiqClient
     def call(worker_class, job, queue, redis_pool)
-      job['tenant_id'] = Multitenancy.current_tenant.tenant_id
-      job['sub_tenant_id'] = Multitenancy.current_tenant.sub_tenant_id
+      unless Multitenancy.current_tenant.nil?
+        job['tenant_id'] = Multitenancy.current_tenant.tenant_id
+        job['sub_tenant_id'] = Multitenancy.current_tenant.sub_tenant_id
+      end
+
       yield
     end
   end
--- lib/multitenancy/middleware/sidekiq_server.rb
+++ lib/multitenancy/middleware/sidekiq_server.rb
@@ -1,8 +1,12 @@
 module Multitenancy::Middleware
   class SidekiqServer
     def call(worker, job, queue)
-      tenant = Multitenancy::Tenant.new(job['tenant_id'], job['sub_tenant_id'])
-      Multitenancy.with_tenant(tenant) do
+      if job['tenant_id']
+        tenant = Multitenancy::Tenant.new(job['tenant_id'], job['sub_tenant_id'])
+        Multitenancy.with_tenant(tenant) do
+          yield
+        end
+      else
         yield
       end
     end
